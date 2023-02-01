c77bcd832c70c2e281adff3e9aae0f1138723770
raeesa99 <raeesapatel99@hotmail.com>
2023-02-01 13:21:25 +0000

    changed links/names

--- app/Nav.js
+++ app/Nav.js
@@ -5,28 +5,22 @@ export default function Nav() {
   return (
     <div class="nav">
       <ul>
-        
-          <Link href="/login" class="nav-link">
-            <li>Login</li>
-          </Link>
-        
-        
-          <Link href="/register" class="nav-link">
-            <li>register</li>
-          </Link>
-        
-        
-          <Link href="/useraccount" class="nav-link">
-            <li>Buyer account</li>
-          </Link>
-
-          <Link href="/selleraccount" class="nav-link">
-            <li>Seller account</li>
-          </Link>
 
           <a href="/" class="nav-link">
             <li>Home</li>
           </a>
+        
+          <Link href="/customer-login" class="nav-link">
+            <li> Customer Login</li>
+          </Link>
+        
+          <Link href="/seller-login" class="nav-link">
+            <li>Event Organiser Login</li>
+          </Link>
+
+          <Link href="/register" class="nav-link">
+            <li>Sign Up</li>
+          </Link>
         
         {/* <li>
           <label for="search"></label>
