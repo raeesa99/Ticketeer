d3aea48683f3c50a96833cb84419192342fa16cc
raeesa99 <raeesapatel99@hotmail.com>
2023-02-01 14:09:42 +0000

    minor changes to forms

--- app/customer-login/page.jsx
+++ app/customer-login/page.jsx
@@ -6,26 +6,23 @@ import  Link from 'next/link';
 export default function Login() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        
+      <div className={styles.description}>  
         <form action='/Login' id='cont1' method='post'>
           <h2 id="login1">Customer Login:</h2>
           <div id='box1'>
-            <label for="person"></label>
+            <label for="email">Email:</label>
+            <br/>
             <input type="text" id="username" name="person" placeholder="username"/>
-            <label for="type"></label>
+
+            <label for="type">Password:</label>
+            <br/>
             <input type="text" id="password" name="type" placeholder="password"/>
+            <br/>
             <input type="submit" value="Submit" id ="submit1"/>
-            <a  id ="reg"href="http://localhost:3000/register">register</a>
-
             
-
-          
+            {/* <a  id ="reg"href="http://localhost:3000/register">register</a> */}
           </div>
-
-
         </form>
- 
       </div>
   </main>
   )
