40b1d3873d2fafdc8684e339cebce88562720f91
T-Sajad <talalsajad300@gmail.com>
2023-02-06 10:35:18 +0000

    fix

---
+++ app/customer-login/page.jsx
@@ -0,0 +1,29 @@
+import styles from './login.css'
+import  Link from 'next/link';
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Login() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>  
+        <form action='/Login' id='cont1' method='post'>
+          <h2 id="login1">Customer Login:</h2>
+          <div id='box1'>
+            <label for="email"></label>
+            <br/>
+            <input type="text" id="username" name="person" placeholder="username"/>
+
+            <label for="type"></label>
+            <br/>
+            <input type="text" id="password" name="type" placeholder="password"/>
+            <br/>
+            <input type="submit" value="Submit" id ="submit1"/>
+            
+            {/* <a  id ="reg"href="http://localhost:3000/register">register</a> */}
+          </div>
+        </form>
+      </div>
+  </main>
+  )
+}
--- src/app/middleware/database.js
+++ src/app/middleware/database.js
@@ -1,21 +1,6 @@
+import mongoose from 'mongoose';
 
-import { MongoClient } from 'mongodb';
-import nextConnect from 'next-connect';
+const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
 
-const client = new MongoClient('mongodb+srv://<admin>:<rcS4abZWARCvYb48>@ticketeer.haho9jx.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+export default connectMongo;
 
-async function database(req, res, next) {
-  if (!client.isConnected()) await client.connect();
-  req.dbClient = client;
-  req.db = client.db('MCT');
-  return next();
-}
-
-const middleware = nextConnect();
-
-middleware.use(database);
-
-export default middleware;
