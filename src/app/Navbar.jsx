6ac6393e787f2dd216870d0c3b5bd330adb0141e
T-Sajad <talalsajad300@gmail.com>
2023-02-07 10:12:40 +0000

    fixed navbar

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -10,51 +10,67 @@ export default function Nav() {
   if (customerLoggedIn) {
     navLinks =
     <>
-        <Link href="/myaccount" className="nav-link">
+        <Link href="/myaccount" class="nav-link">
           <li>My Account</li>
         </Link>
 
-        <Link href="/logout" className="nav-link">
+        <Link href="/logout" class="nav-link">
           <li>Log out</li>
         </Link>
     </>
   } else if (sellerLoggedIn) {
     navLinks =
     <>
-      <Link href="/sellTickets" className="nav-link">
+      <Link href="/sellTickets" classN="nav-link">
             <li>List an event</li>
           </Link>
 
-      <Link href="/logout" className="nav-link">
+      <Link href="/logout" class="nav-link">
               <li>Log out</li>
             </Link>
     </>
   }
   else {
     <>
-    <Link href="/customer-login" className="nav-link">
+    <Link href="/customer-login" class="nav-link">
     <li> Customer Login</li>
   </Link>
 
-  <Link href="/seller-login" className="nav-link">
+  <Link href="/seller-login" class="nav-link">
     <li>Event Organiser Login</li>
   </Link>
 
-  <Link href="/register" className="nav-link">
+  <Link href="/register" class="nav-link">
     <li>Customer Sign Up</li>
   </Link>
 
-  <Link href="/register" className="nav-link">
+  <Link href="/register" class="nav-link">
     <li>Event Organiser Sign Up</li>
   </Link>
   </>
   }
   return (
     <>
-          <a href="/" className="nav-link">
-            <li>Home</li>
+              <div class="nav">
+      <ul>
+        <li>
+          <a href="/Login" class="nav-link">
+            Login
           </a>
-      {navLinks}
+        </li>
+        <li>
+          <a href="/register" class="nav-link">
+            register
+          </a>
+        </li>
+        <li>
+          <a href="/" class="nav-link">
+            Home 
+          </a>
+        </li>
+      </ul>
+    </div>
+      
     </>
   );
 };
