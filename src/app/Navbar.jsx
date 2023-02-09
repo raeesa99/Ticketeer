d9fdfbb4d577998d6b70002bb2f9c19eb5700ea5
raeesa99 <raeesapatel99@hotmail.com>
2023-02-09 16:33:18 +0000

    navbar

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -4,39 +4,40 @@ import  Link from 'next/link';
 import { useState, useEffect } from "react";
 
 export default function Nav() {
-  let [navLinks, setNavLinks] = useState(<></>);
-  let loginType;
-  useEffect(() => {
-    // Perform localStorage action
-    loginType = localStorage.getItem('type')
-  }, [])
+  // let [navLinks, setNavLinks] = useState(<></>);
+  // let loginType;
+  // useEffect(() => {
+  //   // Perform localStorage action
+  //   loginType = localStorage.getItem('type')
+  // }, [])
 
-  if (loginType == 'customer') {
-    setNavLinks(
-    <>
-  <Link href="/my-account" className="nav-link">
-    <li>My Account</li>
-  </Link>
+  // if (loginType == 'customer') {
+  //   setNavLinks(
+  //   <>
+  // <Link href="/my-account" className="nav-link">
+  //   <li>My Account</li>
+  // </Link>
 
-  <Link href="/logout" className="nav-link">
-    <li>Log out</li>
-  </Link>
-    </>
-    )
-  } else if (loginType == 'seller') {
-    setNavLinks(
-    <>
-  <Link href="/sell-tickets" className="nav-link">
-    <li>List an event</li>
-  </Link>
+  // <Link href="/logout" className="nav-link">
+  //   <li>Log out</li>
+  // </Link>
+  //   </>
+  //   )
+  // } else if (loginType == 'seller') {
+  //   setNavLinks(
+  //   <>
+  // <Link href="/sell-tickets" className="nav-link">
+  //   <li>List an event</li>
+  // </Link>
 
-  <Link href="/logout" className="nav-link">
-    <li>Log out</li>
-  </Link>
-    </>
-    )
-  }
+  // <Link href="/logout" className="nav-link">
+  //   <li>Log out</li>
+  // </Link>
+  //   </>
+  //   )
+  // }
   return (
+    <nav className="navbar">
     <>
   <a href="/" className="nav-link">
     <li>Home</li>
@@ -74,7 +75,8 @@ export default function Nav() {
     <li>Log out</li>
   </Link>
 
-  {navLinks}
+  {/* {navLinks} */}
   </>
+  </nav>
   )
 };
